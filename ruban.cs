5304841a7bb6771288db5c732031314ddb72868d
Priyadharshini Balaji <priyadharshini.b@itp.objectfrontier.com>
2023-09-12 16:18:15 +0530

    tata

---
+++ ruban.cs
@@ -0,0 +1,14 @@
+
+internal class ruban
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("i am ruban");
+        Console.WriteLine("hi team");
+        Console.WriteLine("iam laevi");
+        Console.WriteLine("iam karni");
+        Console.WriteLine("Hello Guys...");
+        Console.WriteLine("hiiiiii");
+    }
+}
+
