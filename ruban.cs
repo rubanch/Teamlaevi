140c9bbad336c0a378c44ec935a14268eb78e0fd
Karnisha Karunakaran <karnisha.k@itp.objectfrontier.com>
2023-09-12 16:11:07 +0530

    class

--- ruban.cs
+++ ruban.cs
@@ -1,7 +1,11 @@
-Console.WriteLine("i am ruban");
-Console.WriteLine("hi team");
-Console.WriteLine("iam laevi");
-
-Console.WriteLine("iam karni");
-Console.WriteLine("Hello Guys...");
-
+internal class ruban
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("i am ruban");
+        Console.WriteLine("hi team");
+        Console.WriteLine("iam laevi");
+        Console.WriteLine("iam karni");
+        Console.WriteLine("Hello Guys...");
+    }
+}
